56f01831c003c848079c9924931b9919662450b8
Bimisha Mishra <Dell@DESKTOP-IIVIVRE>
2022-05-08 14:30:59 +0545

    demo database created.

---
+++ Todo_App/app/src/main/java/com/example/todoapp/TodoDao.java
@@ -0,0 +1,23 @@
+package com.example.todoapp;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface TodoDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Todo list);
+
+    @Query("DELETE FROM todo_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM todo_table ORDER BY list ASC")
+    LiveData<List<Todo>> getAlphabetizedWords();
+}
---
+++ Todo_App/app/src/main/java/com/example/todoapp/TodoViewHolder.java
@@ -0,0 +1,27 @@
+package com.example.todoapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TodoViewHolder extends RecyclerView.ViewHolder {
+    private final TextView todoItemView;
+
+    private TodoViewHolder(View itemView){
+        super(itemView);
+        todoItemView = itemView.findViewById(R.id.textView);
+    }
+
+    public void bind(String text) {
+        todoItemView.setText(text);
+    }
+
+    static TodoViewHolder create(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recyclerview_item, parent, false);
+        return new TodoViewHolder(view);
+    }
+}
