b9279fa16f61d7a8975cca6accd37bc547c6138b
Bimisha Mishra <Dell@DESKTOP-IIVIVRE>
2022-05-09 18:38:36 +0545

    checkbox added

---
+++ Todo App/app/src/main/java/com/example/todoapp/TodoDatabase.java
@@ -0,0 +1,54 @@
+package com.example.todoapp;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Todo.class}, version = 1, exportSchema = false)
+public abstract class TodoDatabase extends RoomDatabase {
+    public abstract TodoDao todoDao();
+
+    private static volatile TodoDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static TodoDatabase getDatabase(final Context context) {
+        if(INSTANCE == null){
+            synchronized (TodoDatabase.class){
+                if(INSTANCE == null){
+                    Room.databaseBuilder(context.getApplicationContext(),
+                            TodoDatabase.class, "Todo_database").addCallback(TodoDatabaseCallback);
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                TodoDatabase.class, "Todo_database")
+                                .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback TodoDatabaseCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            databaseWriteExecutor.execute(() -> {
+                TodoDao dao = INSTANCE.todoDao();
+                dao.deleteAll();
+
+                Todo todo = new Todo("Hello");
+                dao.insert(todo);
+                todo = new Todo("World");
+                dao.insert(todo);
+            });
+        }
+    };
+}
